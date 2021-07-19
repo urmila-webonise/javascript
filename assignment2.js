127730bcb6d413c67f2afe154e6248fd69dbd129
Urmila Nale <urmila.nale@weboniselab.com>
2021-07-19 17:57:29 +0530

    performed sorting without using in-built sort

--- assignment2.js
+++ assignment2.js
@@ -1,4 +1,4 @@
-// Assignment No 2
+ // Assignment No 2
 
 // object sort in ascending descending
 // get data from obj which have more then 20 age
@@ -25,6 +25,44 @@ console.log(persons);
 persons.sort(function (a, b) {
     return b.age - a.age;
 });
+console.log(persons);
+
+//ascending order
+
+let persons = [{ age: 1, name: "A" },
+    { age: 2, name: "B" },
+    { age: 13, name: "C" },
+    { age: 42, name: "D" },
+    { age: 42, name: "E" }];
+
+for (let i = 1; i < persons.length; i++){
+    for (let j = 0; j < i; j++){
+        if (persons[i].age < persons[j].age) {
+            let temp = persons[i].age;
+            persons[i].age = persons[j].age;
+            persons[j].age = temp;
+        }
+    }
+}
+console.log(persons);
+
+//descending order
+
+let persons = [{ age: 1, name: "A" },
+    { age: 2, name: "B" },
+    { age: 13, name: "C" },
+    { age: 42, name: "D" },
+    { age: 42, name: "E" }];
+
+for (let i = 1; i < persons.length; i++){
+    for (let j = 0; j < i; j++){
+        if (persons[j].age < persons[i].age) {
+            let temp = persons[j].age;
+            persons[j].age = persons[i].age;
+            persons[i].age = temp;
+        }
+    }
+}
 console.log(persons);
 
 //2. get data from obj which have more then 20 age
