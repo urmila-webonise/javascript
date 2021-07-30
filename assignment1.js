b690661eb4503d188d57ff0efa27733c1ba21a58
Urmila Nale <urmila.nale@weboniselab.com>
2021-07-30 12:22:29 +0530

    performed operations without using sort method

--- assignment1.js
+++ assignment1.js
@@ -5,73 +5,60 @@
 // get max number and second max number from Array
 // [2,1,4,55,6,7,22,0,'',2,1]
 
-// 1.sort array in ascending and  descending 
+// 1.sort array in ascending and  descending
 
-//ascending 
+const numbers = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
 
-let numbers = [2, 1, 4, 55, 6, 7, 22, 0, '', 2, 1];
- 
 numbers.sort(function (a, b) {
-    return a - b;
+  return a - b;
 });
 console.log(numbers);
 
-//descending
-let numbers = [2, 1, 4, 55, 6, 7, 22, 0, '', 2, 1];
+const numbers = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
 
 numbers.sort(function (a, b) {
-    return b - a;
+  return b - a;
 });
 console.log(numbers);
 
-//ascendinng order
+const nums = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
 
-let nums = [2, 1, 4, 55, 6, 7, 22, 0, '', 2, 1];
-
-for (let i = 1; i < nums.length; i++){
-    for (let j = 0; j < i; j++){
-        if (nums[i] < nums[j]) {
-            let temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
+for (let i = 1; i < nums.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (nums[i] < nums[j]) {
+      let temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
     }
+  }
 }
 console.log(nums);
 
-//Decending order
+const nums = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
 
-let nums = [2, 1, 4, 55, 6, 7, 22, 0, '', 2, 1];
-
-for (let i = 1; i < nums.length; i++){
-    for (let j = 0; j < i; j++){
-        if (nums[j] < nums[i]) {
-            let temp = nums[j];
-            nums[j] = nums[i];
-            nums[i] = temp;
-        }
+for (let i = 1; i < nums.length; i++) {
+  for (let j = 0; j < i; j++) {
+    if (nums[j] < nums[i]) {
+      let temp = nums[j];
+      nums[j] = nums[i];
+      nums[i] = temp;
     }
+  }
 }
 console.log(nums);
 
-//Max number
-
-let numbers = [2, 1, 4, 55, 6, 7, 22, 0, '', 2, 1];
+const numbers = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
 
 let max = numbers.sort(function (a, b) {
-    return b - a;
+  return b - a;
 })[0];
 console.log(max);
 
-//By using spread operator
-
-let numbers = [2, 1, 4, 55, 6, 7, 22, 0, '', 2, 1];
+const numbers = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
 
 console.log(Math.max(...numbers));
 
 //2. implement stack and queue in js
-
-//Stack with javascript
 
 let stack = [];
 
@@ -98,8 +85,6 @@ console.log(stack);
 stack.push(1);
 console.log(stack);
 
-//Removing element from stack
-
 stack.pop();
 console.log(stack);
 stack.pop();
@@ -109,51 +94,71 @@ console.log(stack);
 stack.pop();
 console.log(stack);
 
+const Queue = [];
+Queue.push(2);
+console.log(Queue);
+Queue.push(1);
+console.log(Queue);
+Queue.push(4);
+console.log(Queue);
+Queue.push(55);
+console.log(Queue);
+Queue.push(6);
+console.log(Queue);
+Queue.push(7);
+console.log(Queue);
+Queue.push(22);
+console.log(Queue);
+Queue.push(0);
+console.log(Queue);
+Queue.push("");
+console.log(Queue);
+Queue.push(2);
+console.log(Queue);
+Queue.push(1);
+console.log(Queue);
 
-//Queue with javascript
-
-function Queue() {
-    this.element = [];
-}
-//add an element
-Queue.prototype.enqueue = function (e) {
-    this.element.push(e);
-};
-//Remove an ele from front of queue
-
-Queue.prototype.dequeue = function () {
-    this.element.shift();
-};
-//check queue is empty or not
-
-Queue.prototype.isEmpty = function () {
-    this.element.length == 0;
-};
-
-//create a queue
-
-let q = new Queue();
-q.enqueue(2);
-q.enqueue(1);
-q.enqueue(4);
-q.enqueue(55);
-q.enqueue(6);
-q.enqueue(7);
-q.enqueue(22);
-q.enqueue(0);
-q.enqueue("");
-q.enqueue(2);
-q.enqueue(1);
-q.dequeue();
-console.log(q);
-
-
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
+Queue.shift();
+console.log(Queue);
 
 //3. second max number from Array
 
-let numbers = [2, 1, 4, 55, 6, 7, 22, 0, '', 2, 1];
+const numbers = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
 
 let max = numbers.sort(function (a, b) {
-    return b - a;
+  return b - a;
 })[1];
 console.log(max);
+
+const numbers = [2, 1, 4, 55, 6, 7, 22, 0, "", 2, 1];
+let max = 0;
+let secondMax = 0;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > max) {
+    secondMax = max;
+    max = numbers[i];
+  } else if (numbers[i] > secondMax) {
+    secondMax = numbers[i];
+  }
+}
+console.log(secondMax);
